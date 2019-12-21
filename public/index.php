13ba025365d8fc2be51be539f6481113e2e17517
svanlaere <svanlaere@users.noreply.github.com>
2019-12-21 19:38:10 +0100

    Update index.php
    Added settings

--- public/index.php
+++ public/index.php
@@ -4,11 +4,16 @@ declare(strict_types=1);
 
 use Example\App;
 
-// disable all error reporting in production by settings values to 0 (zero)
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+// disable error reporting in production
+error_reporting(0);
+ini_set('display_errors', '0');
 
+// set app default settings
+ini_set('default_charset', 'UTF-8');
+date_default_timezone_set('Europe/Amsterdam');
+setlocale(LC_ALL, 'nl_NL');
+
+// define app paths
 define('DS', DIRECTORY_SEPARATOR);
 define('ABS_PATH', realpath(__DIR__ . '/..'));
 
@@ -16,6 +21,10 @@ define('SRC_PATH', ABS_PATH . DS . 'src');
 define('APP_PATH', SRC_PATH . DS . 'example');
 define('CONFIG_PATH', APP_PATH . DS . 'config');
 define('CACHE_PATH', APP_PATH . DS . 'cache');
+
+// set to true to start route caching
+// if you add or change any routes you need to delete the routes.cache file
+define('ENABLE_ROUTE_CACHING', false);
 
 session_start();
 
