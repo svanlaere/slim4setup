5801e6b6044c7b87b30d290d2fd20135c3de5afa
svanlaere <svanlaere@users.noreply.github.com>
2019-12-22 22:28:35 +0100

    Update index.php
    Simplified setting debug modus

--- public/index.php
+++ public/index.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 use Example\App;
 
 // disable error reporting in production
-error_reporting(0);
-ini_set('display_errors', '0');
+define('DEBUG', true);
+
+// set to true to start route caching
+// if you add or change any routes you need to delete the routes.cache file
+define('ENABLE_ROUTE_CACHING', false);
 
 // set app default settings
 ini_set('default_charset', 'UTF-8');
@@ -21,10 +24,21 @@ define('SRC_PATH', ABS_PATH . DS . 'src');
 define('APP_PATH', SRC_PATH . DS . 'example');
 define('CONFIG_PATH', APP_PATH . DS . 'config');
 define('CACHE_PATH', APP_PATH . DS . 'cache');
+define('DEBUG_PATH', APP_PATH . DS . 'debug');
 
-// set to true to start route caching
-// if you add or change any routes you need to delete the routes.cache file
-define('ENABLE_ROUTE_CACHING', false);
+if (defined('DEBUG')) {
+    ini_set('error_log', DEBUG_PATH . DS . 'error.log');
+    if (DEBUG === true) {
+        error_reporting(E_ALL | E_STRICT);
+        ini_set('display_errors', '1');
+        ini_set('display_startup_errors', '1');
+        ini_set('html_errors', '1');
+    } else if (DEBUG === false) {
+        error_reporting(0);
+        ini_set('display_errors', '0');
+        ini_set('display_startup_errors', '0');
+    }
+}
 
 session_start();
 
